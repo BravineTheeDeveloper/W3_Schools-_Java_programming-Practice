0510507798e4537874ad9f40904369746e4aa9c4
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-10 18:21:37 +0300

    Narrow Casting

---
+++ typeCasting2.java
@@ -0,0 +1,11 @@
+//This program displays how Narrow casting is done in Java
+
+public class typeCasting2 {
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
+      }
+}
