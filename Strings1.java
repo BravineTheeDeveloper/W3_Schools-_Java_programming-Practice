e7148fed22069ec178f04299860ccf1f58537ccc
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-10 18:48:59 +0300

    Strings

--- Strings1.java
+++ Strings1.java
@@ -12,3 +12,9 @@ public class Strings1 {
 //Other string methods available are {toUpperCase()} and {toLowerCase()}
 //The toUpperCase() is used to print the string contents to uppercase letters as the name suggest
 //The toLowerCase() is used to print the content of the string to lowercase letters
+
+//The program below is used to print index (the position) of the first occurrence of a specified text in a string (including whitespace)
+/**
+ *String txt = "Please locate where 'locate' occurs!";
+ *System.out.println(txt.indexOf("locate")); // Outputs 7
+ */
