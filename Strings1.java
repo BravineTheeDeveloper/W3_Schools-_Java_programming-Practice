43bbcefad3164e56f126d4257eeecac854199937
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-10 18:39:01 +0300

    String length

---
+++ Strings1.java
@@ -0,0 +1,10 @@
+//This program helps you identify the length of a string
+
+public class Strings1 {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //The .lenght() extension on the string name is used to print the length of the string
+        System.out.println("The length of the txt string is: " + txt.length());
+      }
+}
