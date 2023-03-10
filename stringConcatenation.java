04464653d99dff5bd379e01e3fde16e8261e20e0
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-10 18:59:34 +0300

    String concatenation

--- stringConcatenation.java
+++ stringConcatenation.java
@@ -2,6 +2,17 @@ public class stringConcatenation {
     public static void main(String [] args){
     String firstName = "John";
     String lastName = "Doe";
+
+    //We have added an empty text (" ") to create a space between firstName and lastName on print.
     System.out.println(firstName + " " + lastName);
     }
 }
+
+
+//You can also use the concat() method to concatenate two strings
+//EXAMPLE
+/**
+ * String firstName = "John ";
+ * String lastName = "Doe";
+ * System.out.println(firstName.concat(lastName));
+*/
