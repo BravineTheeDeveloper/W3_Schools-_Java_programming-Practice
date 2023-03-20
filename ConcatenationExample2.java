79d3c2ddbb2074ee6e9bc7209ee67735e05a24aa
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-20 21:50:31 +0300

    The concat () method!

--- ConcatenationExample2.java
+++ ConcatenationExample2.java
@@ -1,8 +1,8 @@
 public class ConcatenationExample2 {
     public static void main(String[] args) {
         // Declare two string variables
-        String firstName = "John";
-        String lastName = "Doe";
+        String firstName = "Bravine";
+        String lastName = "Bwire";
 
         // Concatenate the two strings using the concat() method
         String fullName = firstName.concat(" ").concat(lastName);
