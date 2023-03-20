924432512206855735de859c3f8ed409051934a0
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-20 21:46:26 +0300

    String concatenation

---
+++ ConcatenationExample1.java
@@ -0,0 +1,13 @@
+public class ConcatenationExample1 {
+    public static void main(String[] args) {
+        // Declare two string variables
+        String str1 = "Hello";
+        String str2 = "World";
+
+        // Concatenate the two strings using the + operator
+        String result = str1 + " " + str2;
+
+        // Print the result
+        System.out.println(result);
+    }
+}
