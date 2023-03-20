2bfb3cf1cc25d45b425761d53a53a89752732565
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-20 21:58:01 +0300

    Which one is greater

---
+++ IfElseExample1.java
@@ -0,0 +1,13 @@
+public class IfElseExample1 {
+    public static void main(String[] args) {
+        // Declare an integer variable
+        int num = 10;
+
+        // Use an if-else statement to check if num is greater than 5
+        if (num > 5) {
+            System.out.println("The number is greater than 5.");
+        } else {
+            System.out.println("The number is less than or equal to 5.");
+        }
+    }
+}
