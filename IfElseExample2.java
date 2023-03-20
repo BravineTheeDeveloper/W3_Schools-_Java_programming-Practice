c69df255ab4d44c3c5017b94223a7d9304cf07f4
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-20 22:00:53 +0300

    Is it raining?

---
+++ IfElseExample2.java
@@ -0,0 +1,13 @@
+public class IfElseExample2 {
+    public static void main(String[] args) {
+      // Declare a boolean variable and set it to true
+      boolean isRaining = true;
+
+      // Use an if-else statement to check if it's raining
+      if (isRaining) {
+          System.out.println("It's raining. Bring an umbrella.");
+      } else {
+          System.out.println("It's not raining. You don't need an umbrella.");
+      }
+   }
+}
