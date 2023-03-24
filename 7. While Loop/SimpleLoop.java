6c61f59edf414e85455325d0779b1f42be6af5ae
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-24 16:37:09 +0300

    Incrementation of 'i' using while loop

---
+++ 7. While Loop/SimpleLoop.java
@@ -0,0 +1,13 @@
+/**
+ * This is a simple Program illustrating the use of a siple while loop.
+ */
+
+public class SimpleLoop {
+    public static void main(String[] args) {
+      int i = 0;
+      while (i < 5) {
+        System.out.println(i);
+        i++; //'i' should be incremented otherwise the loop will never end
+      }
+    }
+  }
