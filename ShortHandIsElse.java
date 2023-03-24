3c265d89a9fb6f75595e1c0c9ff5229fcff27f27
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-24 15:03:26 +0300

    Short hand if else

---
+++ ShortHandIsElse.java
@@ -0,0 +1,10 @@
+//The program below demonstrates the use of a short hand if else in Java
+//It's also known as "ternary operator" because it consists of three operands.
+
+public class ShortHandIsElse {
+public static void main(String[] args){
+    int age = 18;
+    String result = (age >= 18) ? "You are eligible!" : "You are not eligible";
+    System.out.println(result);
+}
+}
