f0ffc1a8c7ff1d62b3b7e3007a198aa865330b37
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-24 17:27:21 +0300

    For loop

---
+++ 8. For loop/forLoop.java
@@ -0,0 +1,12 @@
+/**
+ * This program is an implementation of the Java for loop
+ * It's used When you know exactly how many times you want to loop through a block of code
+ */
+
+public class forLoop {
+    public static void main(String[] args){
+        for (int b = 0; b <= 10; b++){
+            System.out.println(b);
+        }
+    }
+}
