f18047f471887baee118015b8104dbeafd5fd5a6
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-25 20:56:29 +0300

    Break statement

--- 9.Java  Break ORContinue/SimpleBreak.java
+++ 9.Java  Break ORContinue/SimpleBreak.java
@@ -1,4 +1,9 @@
-public class Break {
+/** The "Break" statement
+ * The break statement is used to jump outside of a loop
+ * 
+ */
+
+public class SimpleBreak {
     public static void main(String[] args) {
       for (int b = 0; b < 10; b++) {
         if (b == 4) {
