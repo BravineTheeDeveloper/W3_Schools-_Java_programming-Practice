cd4f6b735aef6e3b51a7eab7d14ba66e2d8c9433
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-25 21:10:22 +0300

    Continue for While and for loops

--- 9.Java  Break ORContinue/SimpleContinue.java
+++ 9.Java  Break ORContinue/SimpleContinue.java
@@ -19,10 +19,11 @@ public class SimpleContinue {
 /**
  * int i = 0;
 while (i < 10) {
+  if (i == 4) {
+    i++;
+    continue;
+  }
   System.out.println(i);
   i++;
-  if (i == 4) {
-    break;
-  }
 }
  */
