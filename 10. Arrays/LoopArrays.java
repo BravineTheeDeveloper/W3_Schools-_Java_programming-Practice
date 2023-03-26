a247cffb9c8ecf57dcf37b57c537b928c95b1e6a
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-27 01:10:55 +0300

    Looping through arrays

---
+++ 10. Arrays/LoopArrays.java
@@ -0,0 +1,14 @@
+/** # Looping Through Arrays!
+ * When looping through an array, we use the "length" property.
+ */
+
+public class LoopArrays {
+    public static void main(String[] args){
+        String [] pals = {"Mark","John","Mason","Paul"};
+
+        //The "pals.length" gets the loop to loop through the entire string there all it's elements will be printed
+        for (int p = 0; p < pals.length; p++){
+            System.out.println(pals[p]);
+        }
+    }
+}
