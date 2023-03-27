ef0530341d93ba8476d35cd4f8111a4daad13dbf
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-03-27 21:31:35 +0300

    Java methods intro

---
+++ 11. Java Methods/Methods.java
@@ -0,0 +1,16 @@
+/**
+ * A method is a block of code which only runs when it is called.
+ * A method must be declared within a class. It is defined with the name of the method,
+ * followed by parentheses ().
+ */
+public class Methods {
+    static void myMethod() {
+      System.out.println("My name is Alison!");
+    }
+
+    public static void main(String[] args) {
+      myMethod();
+      myMethod();
+      myMethod();
+    }
+}
